cf7a3014596cc955814b24d1aaa870751916d00d
rchinna <r.chinnatech@gmail.com>
2026-07-14 19:16:49 +0530

    build the base entity class

--- src/main/java/com/chinnadevtech/amazon/userservice/entity/BaseEntity.java
+++ src/main/java/com/chinnadevtech/amazon/userservice/entity/BaseEntity.java
@@ -1,2 +1,36 @@
-package com.chinnadevtech.amazon.userservice.entity;public class BaseEntity {
+package com.chinnadevtech.amazon.userservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Version;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Version
+    @Column(name = "version", nullable = false)
+    private Long version;
+
 }
